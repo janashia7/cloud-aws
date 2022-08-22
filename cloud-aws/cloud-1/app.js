1f8cf911e0d02d6ebf9614ad20b19f45cf92f09a
Giorgi Janashia <gjanashia@unisens.ge>
2022-08-22 06:22:13 +0400

    add security

--- cloud-aws/cloud-1/app.js
+++ cloud-aws/cloud-1/app.js
@@ -1,10 +1,10 @@
-const axios = require("axios");
+const axios = require('axios');
 
 exports.handler = async (event) => {
   const ip = event.requestContext.http.sourceIp;
   const { data: dataIp } = await axios.get(`http://ip-api.com/json/${ip}`);
   const { data } = await axios.get(
-    `https://api.openweathermap.org/data/2.5/weather?q=${dataIp.city}&appid=669b5487a7cf7957358223f02736c5a8`
+    `https://api.openweathermap.org/data/2.5/weather?q=${dataIp.city}&appid=${API_KEY}`
   );
   return data;
 };
